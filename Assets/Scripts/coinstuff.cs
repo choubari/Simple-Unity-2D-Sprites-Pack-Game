a0018111cd9fd4853b6c473ff3d59c4c3a9ae2db
Kawtar CHOUBARI <choubchouba.kawtar@gmail.com>
2021-04-16 11:51:35 +0000

    add coinstuff

---
+++ Assets/Scripts/coinstuff.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinstuff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	void OnTriggerEnter2D (Collider2D other){
+		Debug.Log("Triggered");
+		Destroy(gameObject);
+	}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
